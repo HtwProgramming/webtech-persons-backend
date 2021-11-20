971a6c3deccd123cd68a04f1402a71f4b0af7371
Daniel Wagner <wagner.daniel87@gmail.com>
2021-11-20 15:36:50 +0100

    remove annotation to demonstrate heroku cli to view the logs

--- src/main/java/de/htwberlin/webtech/persistence/PersonRepository.java
+++ src/main/java/de/htwberlin/webtech/persistence/PersonRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
 
     List<PersonEntity> findAllByFirstName(String firstName);
